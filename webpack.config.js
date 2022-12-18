b39b70ca83bbf2b8dbce89585b82ac030cb852d8
Mariusz <mariuszmalek.dev@gmail.com>
2022-12-18 20:52:52 +0100

    clear code

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,10 @@ const extensionConfig = {
     libraryTarget: 'commonjs2'
   },
   externals: {
-    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    vscode: 'commonjs vscode', // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
     // modules added here also need to be added in the .vscodeignore file
+    bufferutil: "bufferutil",
+    "utf-8-validate": "utf-8-validate",
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
