d70153a436da1237b75cff1c34e293c531b67bfc
Mariusz <mariuszmalek.dev@gmail.com>
2023-04-26 08:53:50 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,45 @@
-import * as vscode from 'vscode';
+import { window, workspace, commands, ExtensionContext, WorkspaceEdit, Uri } from 'vscode';
 import { ChatGPTAPIBrowser } from 'chatgpt';
 
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
   // Create a command that generates unit tests using ChatGPT
-  let disposable = vscode.commands.registerCommand(
+  const disposable = commands.registerCommand(
     'extension.generateUnitTests',
     async () => {
       // Get the current editor
-      const editor = vscode.window.activeTextEditor;
+      const editor = window.activeTextEditor;
       if (!editor) {
-        vscode.window.showErrorMessage('No active editor');
+        window.showErrorMessage('No active editor');
         return;
       }
       
       // Prompt the user for input (e.g. the name of a class or function to create a unit test for)
-      const input = await vscode.window.showInputBox({
+      const input = await window.showInputBox({
         placeHolder: 'Enter the name of a class or function',
       });
 
-      // Use ChatGPT to generate the unit test code
-      const api = new ChatGPTAPIBrowser({
-        email: '',//process.env.OPENAI_EMAIL,
-        password: ''//process.env.OPENAI_PASSWORD
-      })
-      await api.initSession()
-      
-      const result = await api.sendMessage(`Write a unit test for ${input}`)
-      // console.log(result.response)
-      // const unitTestCode = response.data.replies[0];
-      vscode.workspace.openTextDocument({ language: 'typescript' }).then((doc) => {
-        const edit = new vscode.WorkspaceEdit();
-        edit.insert(doc.uri, new vscode.Position(0, 0), result.response);
-        vscode.workspace.applyEdit(edit);
-      });
+      if (!input) {
+        return;
+      }
+
+      try {
+        // Use ChatGPT to generate the unit test code
+        const api = new ChatGPTAPIBrowser({
+          email: process.env.OPENAI_EMAIL || '',
+          password: process.env.OPENAI_PASSWORD || ''
+        });
+        await api.initSession();
+        const result = await api.sendMessage(`Write a unit test for ${input}`);
+        
+        // Insert the generated unit test code at the beginning of a new Typescript file
+        const doc = await workspace.openTextDocument({ language: 'typescript' });
+        const edit = new WorkspaceEdit();
+        edit.insert(doc.uri, new Uri().with({ scheme: 'untitled', path: 'test.ts' }), result.response);
+        workspace.applyEdit(edit);
+        window.showInformationMessage(`Unit test for ${input} generated successfully`);
+      } catch (error) {
+        window.showErrorMessage(`Error generating unit test: ${error.message}`);
+      }
     }
   );
 
